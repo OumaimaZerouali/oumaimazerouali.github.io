8d6ca622f71301ab9a3244f9b67e8921d9ba38e2
oumaimazerouali <oumaima.zerouali@ordina.be>
2025-02-17 08:10:12 +0100

    Version 2

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import {Routes} from '@angular/router';
+import {BrowserComponent} from './browser/browser.component';
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
+import {BookmarksComponent} from './bookmarks/bookmarks.component';
+import {ContactComponent} from './contact/contact.component';
+import {PreferencesComponent} from './preferences/preferences.component';
+import {LogoutComponent} from './logout/logout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BrowserComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'bookmarks', component: BookmarksComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'preferences', component: PreferencesComponent },
+      { path: 'logout', component: LogoutComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
+  { path: '**', redirectTo: 'home' },
+];
---
+++ frontend/src/app/browser/browser.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {NavbarComponent} from '../navbar/navbar.component';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-browser',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    RouterOutlet
+  ],
+  templateUrl: './browser.component.html',
+  styleUrl: './browser.component.scss'
+})
+export class BrowserComponent {
+
+}
